aa4c1d8bda7f334b9675d5d0bcdfb0518368fb96
kannanATexcelforte <kannan@excelforte.com>
2024-01-07 16:19:30 +0530

    07.01.2024-rf
    completed

--- public/main.js
+++ public/main.js
@@ -1,4 +1,21 @@
-console.log('test');
+function init() {
+    const input = document.getElementById('upload');
+    const fileReader = new FileReader();
+
+    fileReader.onloadend = () => {
+        let base64 = fileReader.result.replace(
+            /^data:image\/(png|jpeg|jpg);base64,/, ''
+        );
+        console.log(input.files[0]);
+        console.log(base64);
+    }
+
+    input.addEventListener('change', () => {
+        fileReader.readAsDataURL(input.files[0]);
+    })
+};
+
+init();
 
 // async function init() {
 //   let rustApp = null
